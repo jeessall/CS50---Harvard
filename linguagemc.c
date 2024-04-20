9a7281faaac1030e210dafbd8d74e588ada7fd06
Jéssica <pedroso.jeh12@gmail.com>
2024-04-20 09:05:11 -0300

    aula_1

---
+++ linguagemc.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, world");
+}
+
+//para rodar esse programa usamos ./nome do arquivo EX: ./linguagemc
+
+int main(void) {
+    get_string("whats your name? ");
+    printf("hello, %s\n", answer);
+}
+
+
+
